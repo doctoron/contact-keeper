719b297a6507c625a6dfe916db864cfb75864a8d
doctoron <ron.antonio@me.com>
2020-03-02 13:32:35 -0500

    prepare for heroku deploy

--- config/db.js
+++ config/db.js
@@ -19,15 +19,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-  // mongoose.connect(db, {
-  //   useNewUrlParser: true,
-  //   useCreateIndex: true,
-  //   useFindAndModify: false,
-  //   useUnifiedTopology: true
-  // })
-  //   .then(() => console.log('MongoDB Connected...'))
-  //   .catch(err => {
-  //     console.error(err.message);
-  //     process.exit(1);
-  //   });
